a4db4a4bbdb97c49e1cc67e203c0514a12b3c049
amol1436 <amolwashimkar8888@gmail.com>
2025-07-17 08:24:42 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,21 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.24.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
+// Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyDwAuWv-_EnzyQ1GI_X7Pqnt7VNu07R2kU",
-  authDomain: "focused-sprite-463715-a5.firebaseapp.com",
-  projectId: "focused-sprite-463715-a5",
-  storageBucket: "focused-sprite-463715-a5.firebasestorage.app",
-  messagingSenderId: "518198321332",
-  appId: "1:518198321332:web:4d97a757ec7cfa86a277e1",
-  measurementId: "G-XTF6TSKB9G"
+  apiKey: "AIzaSyDY6fRrW3I-3JntOWM9zWGTBDPj5oZqFz0",
+  authDomain: "agrimitra-36850.firebaseapp.com",
+  projectId: "agrimitra-36850",
+  storageBucket: "agrimitra-36850.firebasestorage.app",
+  messagingSenderId: "838172276534",
+  appId: "1:838172276534:web:69054f06dd8c5010c0a597",
+  measurementId: "G-R7KWCGHLVV"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-// Exporting for use in index.html
-export { auth, provider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+// Export functions
+export { auth, provider, signInWithEmailAndPassword, signInWithPopup };
